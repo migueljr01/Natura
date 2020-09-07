9617e873fd5505b961da921eae204d6a13b14216
migueljr01 <57925019+migueljr01@users.noreply.github.com>
2020-09-07 04:00:53 -0300

    new update

---
+++ Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace Natura
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelEndereco = new System.Windows.Forms.Label();
+            this.textBoxEndereco = new System.Windows.Forms.TextBox();
+            this.textBoxBairro = new System.Windows.Forms.TextBox();
+            this.labelBairro = new System.Windows.Forms.Label();
+            this.textBoxCidade = new System.Windows.Forms.TextBox();
+            this.labelCidade = new System.Windows.Forms.Label();
+            this.labelCEP = new System.Windows.Forms.Label();
+            this.textBoxCEP = new System.Windows.Forms.TextBox();
+            this.labelIdentidade = new System.Windows.Forms.Label();
+            this.textBoxIdentidade = new System.Windows.Forms.TextBox();
+            this.labelCPF = new System.Windows.Forms.Label();
+            this.textBoxCPF = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLimite = new System.Windows.Forms.TextBox();
+            this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnEndereco = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnBairro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCEP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnIdentidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCPF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnLimite = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(1234, 13);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btnIncluir.TabIndex = 2;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(1234, 37);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.btnAtualizar.Location = new System.Drawing.Point(1234, 60);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.TabIndex = 2;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.btnDeletar.Location = new System.Drawing.Point(1234, 83);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletar.TabIndex = 2;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(13, 35);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(640, 26);
+            this.textBoxNome.TabIndex = 3;
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(11, 13);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(49, 19);
+            this.labelNome.TabIndex = 4;
+            this.labelNome.Text = "Nome:";
+            // 
+            // labelEndereco
+            // 
+            this.labelEndereco.AutoSize = true;
+            this.labelEndereco.Location = new System.Drawing.Point(9, 62);
+            this.labelEndereco.Name = "labelEndereco";
+            this.labelEndereco.Size = new System.Drawing.Size(68, 19);
+            this.labelEndereco.TabIndex = 4;
+            this.labelEndereco.Text = "Endereço:";
+            this.labelEndereco.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxEndereco
+            // 
+            this.textBoxEndereco.Location = new System.Drawing.Point(13, 84);
+            this.textBoxEndereco.Name = "textBoxEndereco";
+            this.textBoxEndereco.Size = new System.Drawing.Size(640, 26);
+            this.textBoxEndereco.TabIndex = 3;
+            // 
+            // textBoxBairro
+            // 
+            this.textBoxBairro.Location = new System.Drawing.Point(12, 135);
+            this.textBoxBairro.Name = "textBoxBairro";
+            this.textBoxBairro.Size = new System.Drawing.Size(215, 26);
+            this.textBoxBairro.TabIndex = 3;
+            // 
+            // labelBairro
+            // 
+            this.labelBairro.AutoSize = true;
+            this.labelBairro.Location = new System.Drawing.Point(13, 113);
+            this.labelBairro.Name = "labelBairro";
+            this.labelBairro.Size = new System.Drawing.Size(48, 19);
+            this.labelBairro.TabIndex = 4;
+            this.labelBairro.Text = "Bairro:";
+            this.labelBairro.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxCidade
+            // 
+            this.textBoxCidade.Location = new System.Drawing.Point(230, 135);
+            this.textBoxCidade.Name = "textBoxCidade";
+            this.textBoxCidade.Size = new System.Drawing.Size(214, 26);
+            this.textBoxCidade.TabIndex = 3;
+            // 
+            // labelCidade
+            // 
+            this.labelCidade.AutoSize = true;
+            this.labelCidade.Location = new System.Drawing.Point(231, 113);
+            this.labelCidade.Name = "labelCidade";
+            this.labelCidade.Size = new System.Drawing.Size(54, 19);
+            this.labelCidade.TabIndex = 4;
+            this.labelCidade.Text = "Cidade:";
+            this.labelCidade.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelCEP
+            // 
+            this.labelCEP.AutoSize = true;
+            this.labelCEP.Location = new System.Drawing.Point(451, 113);
+            this.labelCEP.Name = "labelCEP";
+            this.labelCEP.Size = new System.Drawing.Size(36, 19);
+            this.labelCEP.TabIndex = 4;
+            this.labelCEP.Text = "CEP:";
+            this.labelCEP.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxCEP
+            // 
+            this.textBoxCEP.Location = new System.Drawing.Point(450, 135);
+            this.textBoxCEP.Name = "textBoxCEP";
+            this.textBoxCEP.Size = new System.Drawing.Size(203, 26);
+            this.textBoxCEP.TabIndex = 3;
+            // 
+            // labelIdentidade
+            // 
+            this.labelIdentidade.AutoSize = true;
+            this.labelIdentidade.Location = new System.Drawing.Point(677, 13);
+            this.labelIdentidade.Name = "labelIdentidade";
+            this.labelIdentidade.Size = new System.Drawing.Size(77, 19);
+            this.labelIdentidade.TabIndex = 4;
+            this.labelIdentidade.Text = "Identidade:";
+            this.labelIdentidade.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxIdentidade
+            // 
+            this.textBoxIdentidade.Location = new System.Drawing.Point(676, 35);
+            this.textBoxIdentidade.Name = "textBoxIdentidade";
+            this.textBoxIdentidade.Size = new System.Drawing.Size(203, 26);
+            this.textBoxIdentidade.TabIndex = 3;
+            // 
+            // labelCPF
+            // 
+            this.labelCPF.AutoSize = true;
+            this.labelCPF.Location = new System.Drawing.Point(886, 13);
+            this.labelCPF.Name = "labelCPF";
+            this.labelCPF.Size = new System.Drawing.Size(36, 19);
+            this.labelCPF.TabIndex = 4;
+            this.labelCPF.Text = "CPF:";
+            this.labelCPF.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxCPF
+            // 
+            this.textBoxCPF.Location = new System.Drawing.Point(886, 35);
+            this.textBoxCPF.Name = "textBoxCPF";
+            this.textBoxCPF.Size = new System.Drawing.Size(203, 26);
+            this.textBoxCPF.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(677, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Limite de Compras a prazo:";
+            this.label1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxLimite
+            // 
+            this.textBoxLimite.Location = new System.Drawing.Point(676, 84);
+            this.textBoxLimite.Name = "textBoxLimite";
+            this.textBoxLimite.Size = new System.Drawing.Size(203, 26);
+            this.textBoxLimite.TabIndex = 3;
+            // 
+            // ColumnID
+            // 
+            this.ColumnID.HeaderText = "Código";
+            this.ColumnID.Name = "ColumnID";
+            this.ColumnID.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnID.Width = 60;
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            this.ColumnNome.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnNome.Width = 200;
+            // 
+            // ColumnEndereco
+            // 
+            this.ColumnEndereco.HeaderText = "Endereço";
+            this.ColumnEndereco.Name = "ColumnEndereco";
+            this.ColumnEndereco.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnEndereco.Width = 200;
+            // 
+            // ColumnBairro
+            // 
+            this.ColumnBairro.HeaderText = "Bairro";
+            this.ColumnBairro.Name = "ColumnBairro";
+            this.ColumnBairro.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnBairro.Width = 70;
+            // 
+            // ColumnCidade
+            // 
+            this.ColumnCidade.HeaderText = "Cidade";
+            this.ColumnCidade.Name = "ColumnCidade";
+            this.ColumnCidade.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnCidade.Width = 70;
+            // 
+            // ColumnCEP
+            // 
+            this.ColumnCEP.HeaderText = "CEP";
+            this.ColumnCEP.Name = "ColumnCEP";
+            this.ColumnCEP.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnCEP.Width = 70;
+            // 
+            // ColumnIdentidade
+            // 
+            this.ColumnIdentidade.HeaderText = "Identidade";
+            this.ColumnIdentidade.Name = "ColumnIdentidade";
+            this.ColumnIdentidade.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // ColumnCPF
+            // 
+            this.ColumnCPF.HeaderText = "CPF";
+            this.ColumnCPF.Name = "ColumnCPF";
+            this.ColumnCPF.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // ColumnLimite
+            // 
+            this.ColumnLimite.HeaderText = "Limite ( R$ )";
+            this.ColumnLimite.Name = "ColumnLimite";
+            this.ColumnLimite.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColumnLimite.Width = 110;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 642);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnID,
+            this.ColumnNome,
+            this.ColumnEndereco,
+            this.ColumnBairro,
+            this.ColumnCidade,
+            this.ColumnCEP,
+            this.ColumnIdentidade,
+            this.ColumnCPF,
+            this.ColumnLimite});
+            this.dataGridView.Location = new System.Drawing.Point(9, 177);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(1296, 452);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.Text = "dataGridView";
+            this.Controls.Add(this.textBoxLimite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCPF);
+            this.Controls.Add(this.labelCPF);
+            this.Controls.Add(this.textBoxIdentidade);
+            this.Controls.Add(this.labelIdentidade);
+            this.Controls.Add(this.textBoxCEP);
+            this.Controls.Add(this.labelCEP);
+            this.Controls.Add(this.labelCidade);
+            this.Controls.Add(this.textBoxCidade);
+            this.Controls.Add(this.labelBairro);
+            this.Controls.Add(this.textBoxBairro);
+            this.Controls.Add(this.textBoxEndereco);
+            this.Controls.Add(this.labelEndereco);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.dataGridView);
+            this.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Natura";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label labelEndereco;
+        private System.Windows.Forms.TextBox textBoxEndereco;
+        private System.Windows.Forms.TextBox textBoxBairro;
+        private System.Windows.Forms.Label labelBairro;
+        private System.Windows.Forms.TextBox textBoxCidade;
+        private System.Windows.Forms.Label labelCidade;
+        private System.Windows.Forms.Label labelCEP;
+        private System.Windows.Forms.TextBox textBoxCEP;
+        private System.Windows.Forms.Label labelIdentidade;
+        private System.Windows.Forms.TextBox textBoxIdentidade;
+        private System.Windows.Forms.Label labelCPF;
+        private System.Windows.Forms.TextBox textBoxCPF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLimite;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEndereco;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnBairro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCEP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIdentidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCPF;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnLimite;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Natura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            atualizarGrid();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void limparCampos()
+        {
+            textBoxNome.Clear();
+            textBoxEndereco.Clear();
+            textBoxBairro.Clear();
+            textBoxCidade.Clear();
+            textBoxCEP.Clear();
+            textBoxIdentidade.Clear();
+            textBoxCPF.Clear();
+            textBoxLimite.Clear();
+        }
+        private void atualizarGrid()
+        {
+            //Crio a estrutura da conexão com o banco e passa os parametros
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "natura";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            //Realizo a conexão com o banco
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM `clientes` WHERE ativoCliente=1"; //Traz todo mundo da tabela autor
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dataGridView.Rows.Clear();//FAZ LIMPAR A TABELA
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridView.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//Endereço
+                    row.Cells[3].Value = reader.GetString(3);//Bairro
+                    row.Cells[4].Value = reader.GetString(4);//Cidade
+                    row.Cells[5].Value = reader.GetString(5);//CEP
+                    row.Cells[6].Value = reader.GetString(6);//Identidade
+                    row.Cells[7].Value = reader.GetString(7);//CPF
+                    row.Cells[8].Value = reader.GetString(8);//Limite
+                    dataGridView.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+
+                realizaConexacoBD.Close();
+                //MessageBox.Show("Removido com sucesso"); ;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            //Crio a estrutura da conexão com o banco e passa os parametros
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "Natura";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            //Realizo a conexão com o banco
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+                //MessageBox.Show("Conexão Aberta!");
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                comandoMySql.CommandText = "INSERT INTO `clientes` (`idCliente`, `nomeCliente`, `enderecoCliente`, `bairroCliente`, `cidadeCliente`, `cepCliente`, `identidadeCliente`, `cpfCliente`, `limiteCliente`) " + 
+                    "VALUES( '', '" + textBoxNome.Text + "', '" + textBoxEndereco.Text + "', '" + textBoxBairro.Text + "', '" + textBoxCidade.Text + "', '" + textBoxCEP.Text + "', '" + textBoxIdentidade.Text + "', '" + textBoxCPF.Text + "', '" + textBoxLimite.Text + "');";
+                Console.WriteLine(comandoMySql.CommandText);
+                comandoMySql.ExecuteNonQuery();
+                
+                MessageBox.Show("Inserido com sucesso"); //Exibo mensagem de aviso
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+}
